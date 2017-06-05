09e3c9c0ff677c4737c2860f3a37b97d0c266027
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2017-06-05 23:12:15 +1200

    RE-organize

--- spreadsheetToHtml/tableCreater.py
+++ spreadsheetToHtml/tableCreater.py
@@ -3,15 +3,17 @@ import excelConverse
 import styling
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+
 def Main():
-	target = askopenfilename(title = "Choose spreadsheet file.")
-	style = askopenfilename(filetypes =(("Cascading style sheet file", "*.css"),("All Files","*.*")),title = "Choose CSS file.")
-	final = asksaveasfilename(defaultextension=".txt")
-	tbl = excelConverse.excelToList(target)
-	excelConverse.listToHtml(tbl,final)
-	styling.style(final, style)
-	toClipBoard = open(final,"r").read()
-	subprocess.run(['clip.exe'], input=toClipBoard.encode('utf-8'), check=True)
+    target = askopenfilename(title="Choose spreadsheet file.")
+    style = askopenfilename(filetypes=(("Cascading style sheet file", "*.css"), ("All Files", "*.*")),
+                            title="Choose CSS file.")
+    final = asksaveasfilename(defaultextension=".txt")
+    tbl = excelConverse.excelToList(target)
+    excelConverse.listToHtml(tbl, final)
+    styling.style(final, style)
+    toClipBoard = open(final, "r").read()
+    subprocess.run(['clip.exe'], input=toClipBoard.encode('utf-8'), check=True)
+
 
 Main()
-
